c13d80dc48becb91c24c588ed064163ee58127df
Mike North <northm@yahoo-inc.com>
2015-07-07 16:13:43 -0700

    deploy script tweaks

--- lib/try-generate-pages.sh
+++ lib/try-generate-pages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ev
 echo "Attempting to generate pages"
-if [[ "${TRAVIS_PULL_REQUEST}" = "false" && "${TRAVIS_BRANCH}" == 'master' ]]; then
-  ember github-pages:commit --message "New release" --branch "gh-pages"
+if [[ "${TRAVIS_PULL_REQUEST}" = "false" && "${TRAVIS_BRANCH}" == "master" ]]; then
+  ember github-pages:commit --message "New release" --branch gh-pages
   git push deploy gh-pages  2>&1 >/dev/null
 else
   echo "Not a master build -- no deploy"
