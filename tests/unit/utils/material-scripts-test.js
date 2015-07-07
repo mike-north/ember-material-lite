906ff5b60701a009d33c844c93ca3f4626231a99
Mike North <northm@yahoo-inc.com>
2015-07-07 15:53:58 -0700

    a test

---
+++ tests/unit/utils/material-scripts-test.js
@@ -0,0 +1,12 @@
+import { module, test } from 'qunit';
+
+module('Unit | Utility | material scripts');
+
+// Replace this with your real tests.
+test('material design light scripts are on window', function(assert) {
+  assert.ok(window.MaterialButton, 'MaterialButton is loaded');
+  assert.ok(window.MaterialCheckbox, 'MaterialCheckbox is loaded');
+  assert.ok(window.MaterialDataTable, 'MaterialDataTable is loaded');
+  assert.ok(window.MaterialTabs, 'MaterialTabs is loaded');
+  assert.ok(window.MaterialTab, 'MaterialTab is loaded');
+});
