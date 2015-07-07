079e28aba5de458572a9faa1907fdd5387e602bb
Mike North <northm@yahoo-inc.com>
2015-07-07 16:00:44 -0700

    ember try against 1.11 and 1.12

--- config/ember-try.js
+++ config/ember-try.js
@@ -5,6 +5,24 @@ module.exports = {
       dependencies: { }
     },
     {
+      name: 'ember-1.11',
+      dependencies: {
+        'ember': '~1.11.0'
+      },
+      resolutions: {
+        'ember': '~1.11.0'
+      }
+    },
+    {
+      name: 'ember-1.12',
+      dependencies: {
+        'ember': '~1.12.0'
+      },
+      resolutions: {
+        'ember': '~1.12.0'
+      }
+    },
+    {
       name: 'ember-release',
       dependencies: {
         'ember': 'components/ember#release'
