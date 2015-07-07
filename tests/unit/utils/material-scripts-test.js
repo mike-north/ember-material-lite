8bc81889a1ca0730334e0f039443e5e058a1a14b
Mike North <northm@yahoo-inc.com>
2015-07-07 15:55:08 -0700

    touch

--- tests/unit/utils/material-scripts-test.js
+++ tests/unit/utils/material-scripts-test.js
@@ -9,4 +9,5 @@ test('material design light scripts are on window', function(assert) {
   assert.ok(window.MaterialDataTable, 'MaterialDataTable is loaded');
   assert.ok(window.MaterialTabs, 'MaterialTabs is loaded');
   assert.ok(window.MaterialTab, 'MaterialTab is loaded');
+  assert.ok(window.MaterialTooltip, 'MaterialTooltip is loaded');
 });
