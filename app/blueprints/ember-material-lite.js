f46edb101c7533174d099d2f617c5540f7d60ff3
Mike North <northm@yahoo-inc.com>
2015-07-07 11:51:14 -0700

    Blueprint

---
+++ app/blueprints/ember-material-lite.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-material-lite/blueprints/ember-material-lite';
---
+++ blueprints/ember-material-lite/index.js
@@ -0,0 +1,18 @@
+var RSVP = require('rsvp');
+
+module.exports = {
+  description: 'install ember-material-lite into a typical projet'
+
+  beforeInstall: function (options) {
+    return RSVP.all([
+      this.addBowerPackageToProject('material-design-lite', '1.0.0'),
+      this.addBowerPackageToProject('material-design-lite-src', 'google/material-design-lite#37286ae139b0ae336a8a7e9ed3b18ace1abff7ed')
+    ]);
+  },
+
+  afterInstall: function () {
+    return RSVP.all([
+      this.addPackageToProject('ember-cli-sass', '^3.3.0')
+    ]);
+  }
+};
