d270e75eecb3d81f1d4d3460681a7ed83e6c9a96
Mike North <northm@yahoo-inc.com>
2015-07-07 15:10:21 -0700

    blueprint fixes

--- index.js
+++ index.js
@@ -8,5 +8,7 @@ module.exports = {
   	this._super.included(app);
 
   	app.import(app.bowerDirectory + '/material-design-lite/material.js');
+  	app.import(app.bowerDirectory + '/material-design-lite/material.css', { destDir: 'material-design-lite/css' });
+
   }
 };
