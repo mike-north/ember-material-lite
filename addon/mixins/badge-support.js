51c2202ba4e412b547a2207d196377f480fc4bf9
Mike North <northm@yahoo-inc.com>
2015-07-20 09:38:06 -0700

    Add badge backgrounds

--- addon/mixins/badge-support.js
+++ addon/mixins/badge-support.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
   badge: null,
+  hasBadgeBackground: true,
   attributeBindings: ['badge:data-badge'],
-  classNameBindings: ['badge:mdl-badge']
+  classNameBindings: ['badge:mdl-badge', 'hasBadgeBackground::mdl-badge--no-background']
 });
