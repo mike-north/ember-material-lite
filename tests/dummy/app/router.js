e1e18edf4d511aecad1e6055ed90c8d9a26535c9
Mike North <northm@yahoo-inc.com>
2015-07-21 12:29:47 -0700

    Foundations for inputs

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -14,6 +14,7 @@ Router.map(function() {
   this.route('cards');
   this.route('progress');
   this.route('footer');
+  this.route('textfields');
   this.route('toggles');
   this.route('tooltips');
 });
