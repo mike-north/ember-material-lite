15b8ab5c7d28f0648478e0b4735a443752dd63e8
Mike North <northm@yahoo-inc.com>
2015-07-21 17:17:44 -0700

    Slider

---
+++ tests/integration/components/mdl-slider-test.js
@@ -0,0 +1,18 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('mdl-slider', 'Integration | Component | mdl slider', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  assert.expect(1);
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{mdl-slider}}`);
+
+  assert.equal(this.$().text(), '');
+
+});
