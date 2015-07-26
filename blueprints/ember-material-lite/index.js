c401cd8d2a684f6fab00b3a350e299c6139202dc
Mike North <michael.l.north@gmail.com>
2015-07-26 12:46:15 -0700

    Fix blueprint

--- blueprints/ember-material-lite/index.js
+++ blueprints/ember-material-lite/index.js
@@ -5,18 +5,13 @@ module.exports = {
 
   beforeInstall: function (options) {
     return RSVP.all([
-      this.addBowerPackageToProject('material-design-lite', '1.0.0')
+      this.addBowerPackageToProject('material-design-lite', '~1.0.1')
     ]);
   },
 
   afterInstall: function () {
-    this.ui.writeLine('\n\n\nIf you wish to work with SASS, please add the following to your bower.json');
-    this.ui.writeLine("'material-design-lite-src': 'https://github.com/google/material-design-lite.git#37286ae139b0ae336a8a7e9ed3b18ace1abff7ed',");
-    this.ui.writeLine('\n\n\nAdd the following to your app/index.html for icons to work');
-    this.ui.writeLine('<link rel="stylesheet" href="//fonts.googleapis.com/icon?family=Material+Icons">\n');
     return RSVP.all([
       this.addPackageToProject('ember-cli-sass', '^3.3.0')
     ]);
-
   }
 };
