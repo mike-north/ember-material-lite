8f123ea113f5124b5dc0753e22a6f45919666383
Mike North <michael.l.north@gmail.com>
2015-07-26 13:12:53 -0700

    Guidance for ugpgraders in blueprint

--- blueprints/ember-material-lite/index.js
+++ blueprints/ember-material-lite/index.js
@@ -1,4 +1,5 @@
-var RSVP = require('rsvp');
+var RSVP  = require('rsvp');
+var chalk = require('chalk');
 
 module.exports = {
   description: 'install ember-material-lite into a typical project',
@@ -12,7 +13,13 @@ module.exports = {
   afterInstall: function () {
     return RSVP.all([
       this.addPackageToProject('ember-cli-sass', '^3.3.0'),
-      this.addPackageToProject('ember-new-computed', '~1.0.0')
-    ]);
+      this.addPackageToProject('ember-new-computed', '~1.0.0'),
+      this.addPackageToProject('ember-legacy-views', '~0.2.0')
+    ]).then(function () {
+      this.ui.writeLine('\n' +
+        chalk.bgGreen.black('NOTICE: If you have a reference to "material-design-lite-src" in your bower.json, you may now remove it safely.') +
+        '\n'
+      );
+    }.bind(this));
   }
 };
