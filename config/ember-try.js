41e68fe5b38e0474bceb941738a3f90c4981cc81
Mike North <northm@yahoo-inc.com>
2015-08-18 14:32:40 -0700

    Changes for MDL 1.0.3

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,46 +7,55 @@ module.exports = {
     {
       name: 'ember-1.11',
       dependencies: {
-        'ember': '~1.11.0'
+        ember: '~1.11.0'
       },
       resolutions: {
-        'ember': '~1.11.0'
+        ember: '~1.11.0'
       }
     },
     {
       name: 'ember-1.12',
       dependencies: {
-        'ember': '~1.12.0'
+        ember: '~1.12.0'
       },
       resolutions: {
-        'ember': '~1.12.0'
+        ember: '~1.12.0'
+      }
+    },
+    {
+      name: 'ember-1.13',
+      dependencies: {
+        ember: '~1.13.0'
+      },
+      resolutions: {
+        ember: '~1.13.0'
       }
     },
     {
       name: 'ember-release',
       dependencies: {
-        'ember': 'components/ember#release'
+        ember: 'components/ember#release'
       },
       resolutions: {
-        'ember': 'release'
+        ember: 'release'
       }
     },
     {
       name: 'ember-beta',
       dependencies: {
-        'ember': 'components/ember#beta'
+        ember: 'components/ember#beta'
       },
       resolutions: {
-        'ember': 'beta'
+        ember: 'beta'
       }
     },
     {
       name: 'ember-canary',
       dependencies: {
-        'ember': 'components/ember#canary'
+        ember: 'components/ember#canary'
       },
       resolutions: {
-        'ember': 'canary'
+        ember: 'canary'
       }
     }
   ]
--- tests/unit/utils/material-scripts-test.js
+++ tests/unit/utils/material-scripts-test.js
@@ -6,7 +6,6 @@ module('Unit | Utility | material scripts');
 test('material design light scripts are on window', function(assert) {
   assert.ok(window.MaterialButton, 'MaterialButton is loaded');
   assert.ok(window.MaterialTabs, 'MaterialTabs is loaded');
-  assert.ok(window.MaterialTab, 'MaterialTab is loaded');
   assert.ok(window.MaterialRipple, 'MaterialRipple is loaded');
   assert.ok(window.MaterialLayout, 'MaterialLayout is loaded');
   assert.ok(window.MaterialSpinner, 'MaterialSpinner is loaded');
