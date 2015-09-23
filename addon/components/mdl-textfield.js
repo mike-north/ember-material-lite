fc74171b29a1a05ccf4e4c50d9e83f6c34a8daaf
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2015-09-23 08:12:38 +0200

    feat: move input validations to base-input-component
    Added integration tests for textarea
    Moved common input validations to base-input-component

--- addon/components/mdl-textfield.js
+++ addon/components/mdl-textfield.js
@@ -1,16 +1,8 @@
-import Ember from 'ember';
 import BaseComponent from './-base-input-component';
 import layout from '../templates/components/mdl-textfield';
 
-const {
-  isPresent,
-  observer,
-  run
-} = Ember;
-
 export default BaseComponent.extend({
   pattern: null,
-  errorMessage: null,
   isExpandable: false,
   expandableIcon: null,
   layout,
@@ -19,22 +11,5 @@ export default BaseComponent.extend({
   ],
   beforeMdlInit() {
     this.$('label.mdl-button').attr('for', this.get('_inputId'));
-  },
-  _checkValidity: observer('errorMessage', function() {
-    run.scheduleOnce('afterRender', this, this._setValidity);
-  }),
-  _setValidity() {
-    if (this.isDestroyed) {
-      return;
-    }
-
-    let mdlComponent = this.get('_mdlComponent');
-    let errorMessage = this.get('errorMessage');
-
-    if (isPresent(errorMessage)) {
-      mdlComponent.input_.setCustomValidity(errorMessage.toString());
-    } else {
-      mdlComponent.input_.setCustomValidity('');
-    }
   }
 });
