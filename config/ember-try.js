5c4a050637753f30fb34d8aa8764732ea9d45355
Mike North <michael.l.north@gmail.com>
2018-11-10 10:00:37 -0800

    fix: update dependencies

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,7 +1,5 @@
-/* eslint-env node */
 'use strict';
 
-// eslint-disable-next-line node/no-missing-require
 const getChannelURL = require('ember-source-channel-url');
 
 module.exports = function() {
@@ -9,16 +7,14 @@ module.exports = function() {
     getChannelURL('release'),
     getChannelURL('beta'),
     getChannelURL('canary')
-  ]).then(urls => {
+  ]).then((urls) => {
     return {
       useYarn: true,
       scenarios: [
         {
           name: 'ember-lts-2.16',
           env: {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({
-              'jquery-integration': true
-            })
+            EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true }),
           },
           npm: {
             devDependencies: {
@@ -30,9 +26,7 @@ module.exports = function() {
         {
           name: 'ember-lts-2.18',
           env: {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({
-              'jquery-integration': true
-            })
+            EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true }),
           },
           npm: {
             devDependencies: {
